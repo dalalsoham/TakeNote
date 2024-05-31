1661a91a5ab2fc3c34955e81b33551113820d037
mr_sd_jack_003 <barson0habra@gmail.com>
2024-05-31 15:05:48 +0530

    done with profile icon and name and also done with navigation

--- frontend/takenotes-app/src/components/Navbar/Navbar.jsx
+++ frontend/takenotes-app/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import ProfileInfo from '../Cards/ProfileInfo'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+  const navigate = useNavigate;
+
+  const onLogout = () => {
+    navigate("/login");
+  }
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow border-b border-black-300">
         <h2 className="text-xl font-medium text-primary py-2">TakeNote</h2>
+
+        <ProfileInfo onLogout={onLogout}/>
     </div>
 
   )
