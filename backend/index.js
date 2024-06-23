3fa098584413dd0ebca1e2d1253d38801413e144
mr_sd_jack_003 <barson0habra@gmail.com>
2024-06-23 15:18:46 +0530

    done with notes fetch api

--- backend/index.js
+++ backend/index.js
@@ -192,6 +192,28 @@ app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
     }
 });
 
+//get all notes
+app.get("/get-all-notes", authenticateToken, async (req, res) =>{
+    const {user} = req.user;
+
+    try{
+        const notes = await Note.find({userId: user._id}).sort({isPinned: -1});
+
+        return res.json({
+            error: false,
+            notes,
+            message: "All notes retrieved successfully",
+        });
+    }catch(error){
+        return res.status(500).json({
+            error: true,
+            message: "Internal server error",
+        });
+
+    }
+})
+
+
 app.listen(8000);
 
 module.exports = app;
