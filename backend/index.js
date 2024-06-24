b0ee88250e12779b8175043137178049cedda6d9
mr_sd_jack_003 <barson0habra@gmail.com>
2024-06-24 12:47:34 +0530

    done with the update pinned note

--- backend/index.js
+++ backend/index.js
@@ -238,6 +238,43 @@ app.delete("/delete-note/:noteId", authenticateToken, async (req, res) =>{
     }
 });
 
+//update ispinned note
+app.put("/update-note-pinned/:noteId", authenticateToken, async (req, res) => {
+    const noteId = req.params.noteId;
+    const {isPinned} = req.body;
+    const {user} = req.user;
+
+    // if(!isPinned) {
+    //     return res
+    //     .status(400)
+    //     .json({error: true, message: "No changes provided"});
+    // }
+
+    try{
+        const note = await Note.findOne({_id: noteId, userId: user._id});
+
+        if(!note) {
+            return res.status(404).json({error: true, message: "Note not found"});
+        }
+        
+        // if(isPinned) note.isPinned = isPinned || false;
+        if (isPinned !== undefined) note.isPinned = isPinned;
+
+        await note.save();
+
+        return res.json({
+            error: false,
+            note,
+            message: "Note updated successfully",
+        });
+    }catch(error){
+        return res.status(500).json({
+            error: true,
+            message: "Internal server error",
+        });
+    }
+})
+
 app.listen(8000);
 
 module.exports = app;
