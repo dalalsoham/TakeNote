997bb31bfcb75014eccceff11151975bc83e822a
mr_sd_jack_003 <barson0habra@gmail.com>
2024-06-24 18:12:55 +0530

    login api setup and integration done with frontend

--- frontend/takenotes-app/src/pages/Login/Login.jsx
+++ frontend/takenotes-app/src/pages/Login/Login.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PasswordInput from "../../components/Input/PasswordInput";
 import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
   const handleLogin = async (e) => {
     e.preventDefault();
 
@@ -21,6 +25,29 @@ function Login() {
       return;
     }
     setError("");
+
+
+    //login api call
+    try{
+      const response = await axiosInstance.post("/login", {
+        email: email,
+        password: password,
+      });
+
+      //successful login response
+      if(response.data && response.data.accessToken){
+        localStorage.setItem("token", response.data.accessToken);
+        navigate('/home')
+      }
+
+    }catch(error){
+      //handle login error
+      if(error.response && error.response.data && error.response.data.message){
+        setError(error.response.data.message);
+      }else{
+        setError("An unexpected error occurred. Please try again.");
+      }
+    }
   };
 
   return (
