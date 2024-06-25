3ecbf237e9f137dd90e66db78a2c4401f7062de4
mr_sd_jack_003 <barson0habra@gmail.com>
2024-06-26 01:36:24 +0530

    search note api done

--- backend/index.js
+++ backend/index.js
@@ -291,6 +291,40 @@ app.get("/get-user", authenticateToken, async (req, res) =>{
     });
 });
 
+//search note api
+app.get("/search-notes/", authenticateToken, async (req, res) => {
+    const {user} = req.user;
+    const {query} = req.query;
+
+    if(!query) {
+        return res
+        .status(400)
+        .json({error: true, message: "Search query is required"});
+    }
+
+    try{
+        const matchingNotes = await Note.find({
+            userId: user._id,
+            $or: [
+                {title: {$regex: new RegExp(query, "i")}},
+                {content: {$regex: new RegExp(query, "i")}},
+            ],
+        });
+
+        return res.json({
+            error: false,
+            notes: matchingNotes,
+            message: "Notes matching the search query retrieved successfully.",
+        });
+
+    }catch(error) {
+        return res.status(500).json({
+            error: true,
+            message: "Internal server error.",
+        });
+    }
+});
+
 app.listen(8000);
 
 module.exports = app;
