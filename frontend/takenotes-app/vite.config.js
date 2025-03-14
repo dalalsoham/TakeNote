fa43059273a2425ef97743e3e733ce1f6c66f58b
dalalsoham <barson0habra@gmail.com>
2025-03-14 14:18:15 +0530

    update vite config-1

--- frontend/takenotes-app/vite.config.js
+++ frontend/takenotes-app/vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  define: {
-    __APP_ENV__: process.env.VITE_VERCEL_ENV,
-  }
-})
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'dist'
+    },
+    define: {
+      __APP_ENV__: process.env.VITE_VERCEL_ENV,
+    }
+  };
+});
